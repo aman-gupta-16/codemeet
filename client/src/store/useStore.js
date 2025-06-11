ea1003e1cf548dfa28771ddf3b48e8ef784afb75
Aman Gupta <ag8535467@gmail.com>
2025-06-11 13:45:05 +0530

    video call functioanlity is done but  not tested yet

--- client/src/store/useStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-export const useStore = create((set) => ({
-  bears: 10,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
-}))
--- server/index.js
+++ server/index.js
@@ -15,11 +15,27 @@ const socketidToEmailMap = new Map();
 io.on("connection", (socket) => {
   console.log(`socket is connected : ${socket.id}`);
   socket.on("room:join", ({ email, room }) => {
-    console.log(email,room)
+    console.log(email, room);
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
-    io.to(socket.id).emit("room:join", {email,room})
+    io.to(socket.id).emit("room:join", { email, room });
     socket.join(room);
-    io.to(room).emit("user:joined",{email,id:socket.id})
+    io.to(room).emit("user:joined", { email, id: socket.id });
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
 });
